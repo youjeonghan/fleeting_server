d9ab76616c01dc2dbdac2d4b22ef558566edb569
유정한 <yjh9360@naver.com>
2023-07-01 21:15:15 +0900

    Fleeting 어플 네이밍 변경

--- src/main/kotlin/com/example/blinddateserver/BlindDateServerApplication.kt
+++ src/main/kotlin/com/example/blinddateserver/BlindDateServerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BlindDateServerApplication
+class FleetingServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<BlindDateServerApplication>(*args)
+	runApplication<FleetingServerApplication>(*args)
 }
--- src/test/kotlin/com/example/blinddateserver/BlindDateServerApplicationTests.kt
+++ src/test/kotlin/com/example/blinddateserver/BlindDateServerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BlindDateServerApplicationTests {
+class FleetingServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
